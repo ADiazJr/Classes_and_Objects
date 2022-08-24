e2b9bf047609aea726006d55ef83ac06c84a3841
unknown <arturodiazmay@gmail.com>
2022-08-24 17:02:02 -0500

    feat. completed new classes and called them > bug. changed functions to make them work correctly inside of customer() > and shopping_cart() classes

--- customer.py
+++ customer.py
@@ -8,5 +8,7 @@ class Customer():
         self.shopping_cart.add_to_cart(product)
     
     def view_products(self):
+        if len(self.shopping_cart.cart_products) == 0:
+            print('Shopping Cart is empty')
         for index in range(len(self.shopping_cart.cart_products)):
             print(self.shopping_cart.cart_products[index].products_name)
--- shopping_cart.py
+++ shopping_cart.py
@@ -1,14 +1,17 @@
 from product import Product
 class Shopping_Cart():
-    def __init__(self, products):
-        self.cart_products = [products]
+    def __init__(self):
+        self.cart_products = []
 
-    def calculate_total(self, products):
-        print(products.price)
+    def calculate_total(self):
+        cart_total = 0
+        for index in range(len(self.cart_products)):
+            cart_total += self.cart_products[index].price
+        return cart_total
 
     def add_to_cart(self, products):
         self.cart_products.append(products)
 
     def clear_cart(self):
         while len(self.cart_products) != 0:
-            self.cart_products.remove()
+            self.cart_products.pop(0)
