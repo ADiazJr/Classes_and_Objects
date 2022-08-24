bad50bf1a9887d671083c22150067365f525babc
unknown <arturodiazmay@gmail.com>
2022-08-24 14:44:18 -0500

    refactor. changed the way the set_alarm_on() function works, now takes in parameter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from alarm_clock import Alarm_clock
 
-first_alarm = Alarm_clock()
+first_alarm = Alarm_clock(True)
 first_alarm.set_current_time()
-first_alarm.set_alarm_on()
+first_alarm.set_alarm_on(True)
 first_alarm.set_alarm_time()
